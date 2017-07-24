647c9ec0da0e573f910efb9fcf8d77a7e4a6d35d
Kjartan Skjervøy <kskjer@gmail.com>
2017-07-24 21:27:33 +0200

    Some string utilities

---
+++ MipsSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MipsSharp
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Duplicates a string <paramref name="count"/> times.
+        /// </summary>
+        public static string Dup(this string input, int count) =>
+            string.Join("", Enumerable.Range(0, count).Select(x => input));
+
+        public static string Join(this IEnumerable<string> input, string separator) =>
+            string.Join(separator, input);
+    }
+}
